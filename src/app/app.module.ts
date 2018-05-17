397778453408578740bef15e8151b4431ef3da02
unknown <rayeejaya@gmail.com>
2018-05-17 18:53:59 -0400

    home component is added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms'
 import { AppComponent } from './app.component';
 import { ServersComponent } from './servers/servers.component';
 import { ServerComponent } from './server/server.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServersComponent,
-    ServerComponent
+    ServerComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
